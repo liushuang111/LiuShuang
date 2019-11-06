beea2686ce4146bd2d29485e06a065c53df0b5b0
liushuang111 <358079506@qq.com>
2019-11-06 15:34:21 +0800

    新提交的项目

---
+++ my-propject/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="(item,index) in list" :key="index">
+      <div>{{item.one}}</div>
+      <div>{{item.two}}</div>
+      <div>{{item.three}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Mock from '../mock/mock'
+export default {
+    data() {
+      return {
+        list:[1,2,5,6]
+      }
+    },
+    created(){
+      axios.get('/app').then((res)=>{
+        console.log(res.data)
+      })
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-propject/src/mock/mock.js
@@ -0,0 +1,4 @@
+import Mock from "mockjs"
+import MockJson from './mock.json'
+
+Mock.mock('/app',MockJson)
